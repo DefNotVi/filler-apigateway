fed84a14ec7473ca787aedcb46c966ab6abbdd7a
DefNotVi <vice.escobar@duocuc.cl>
2026-06-24 17:32:59 -0400

    first commit

---
+++ src/main/java/cl/apigateway/filler/GatewayApplication.java
@@ -0,0 +1,12 @@
+package cl.apigateway.filler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
